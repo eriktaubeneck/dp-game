d5183b042df19034a60fa5398478f763df821796
Richa Jain <richajain.dce@gmail.com>
2023-10-09 22:06:30 +0800

    Adding initial page to input number of impressions and conversion rate

--- dp-game-webapp/app/page.tsx
+++ dp-game-webapp/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import Header from '@/app/nav'
-
+import Link from 'next/link';
 
 export default function Example() {
 
@@ -12,31 +12,27 @@ export default function Example() {
       <main className="isolate">
         {/* Hero section */}
         <div className="relative isolate -z-10 overflow-hidden bg-gradient-to-b from-indigo-100/20 pt-14">
-          <div
-            className="absolute inset-y-0 right-1/2 -z-10 -mr-96 w-[200%] origin-top-right skew-x-[-30deg] bg-white shadow-xl shadow-indigo-600/10 ring-1 ring-indigo-50 sm:-mr-80 lg:-mr-96"
-            aria-hidden="true"
-          />
-          <div className="mx-auto max-w-7xl px-6 py-32 sm:py-40 lg:px-8">
-            <div className="mx-auto max-w-2xl lg:mx-0 lg:grid lg:max-w-none lg:grid-cols-2 lg:gap-x-16 lg:gap-y-6 xl:grid-cols-1 xl:grid-rows-1 xl:gap-x-8">
-              <h1 className="max-w-2xl text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl lg:col-span-2 xl:col-auto">
+          <div className="mx-auto max-w-10xl px-6 py-32 sm:py-40 lg:px-8">
+              <h1 className="max-w-xl text-xl font-bold tracking-tight text-gray-900 sm:text-6xl lg:col-span-2 xl:col-auto">
                 Welcome to the Differential Privacy Game!
               </h1>
+              <br />
               <div className="mt-6 max-w-xl lg:mt-0 xl:col-end-1 xl:row-start-1">
                 <p className="text-lg leading-8 text-gray-600">
-                  Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt
-                  amet fugiat veniam occaecat fugiat aliqua. Anim aute id magna aliqua ad ad non deserunt sunt. Qui
-                  irure qui lorem cupidatat commodo.
+                We are doing a research to understand effect of adding DP to your conversion results.
+                We will ask you a bunch of questions and that would help in determining the utility
+                of DP.
                 </p>
               </div>
-              <img
-                src="https://images.unsplash.com/photo-1567532900872-f4e906cbf06a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1280&q=80"
-                alt=""
-                className="mt-10 aspect-[6/5] w-full max-w-lg rounded-2xl object-cover sm:mt-16 lg:mt-0 lg:max-w-none xl:row-span-2 xl:row-end-2 xl:mt-36"
-              />
-            </div>
           </div>
-          <div className="absolute inset-x-0 bottom-0 -z-10 h-12 bg-gradient-to-t from-white sm:h-32" />
         </div>
+        <br />
+        <div className="hidden lg:flex lg:flex-1 lg:justify-center">
+          <Link href="/game" className="text-2xl font-semibold leading-6 text-blue-600">
+            Let's Play! <span aria-hidden="true">&rarr;</span>
+          </Link>
+        </div>
+        <br /><br /><br />
       </main>
     </div>
   )
