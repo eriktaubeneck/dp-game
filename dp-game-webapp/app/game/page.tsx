c995ecf0e740c56318d9b818057d594558eb5d52
Erik Taubeneck <erik.taubeneck@gmail.com>
2023-10-10 14:09:29 -0700

    removed unused import

--- dp-game-webapp/app/game/page.tsx
+++ dp-game-webapp/app/game/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import React, { useState } from "react";
 import { CampaignSizeSlider, ConversionRateSlider } from "./slider";
-import { simulatedPercentiles } from "./simulate";
 import { ArrowRightCircleIcon } from "@heroicons/react/24/outline";
 
 export default function Game() {
