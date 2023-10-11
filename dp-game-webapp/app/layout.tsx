a2cd55e7099d59f7fb105ad8dd20d038a0ba520b
Erik Taubeneck <erik.taubeneck@gmail.com>
2023-10-11 10:26:54 -0700

    update links, remove login for now

--- dp-game-webapp/app/layout.tsx
+++ dp-game-webapp/app/layout.tsx
@@ -1,18 +1,19 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import Header from '@/app/nav'
+import "./globals.css";
+import React from "react";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import Header from "@/app/nav";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'DP Game',
-}
+  title: "DP Game",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
@@ -20,7 +21,7 @@ export default function RootLayout({
         <div className="bg-white">
           {/* Header */}
           <Header />
-          
+
           <main className="isolate">
             {/* Hero section */}
             <div className="relative isolate -z-10 overflow-hidden bg-gradient-to-b min-h-screen from-indigo-100/20 pt-14">
@@ -28,12 +29,12 @@ export default function RootLayout({
                 className="absolute inset-y-0 right-1/2 -z-10 -mr-96 w-[200%] origin-top-right skew-x-[-30deg] bg-white shadow-xl shadow-indigo-600/10 ring-1 ring-indigo-50 sm:-mr-80 lg:-mr-96"
                 aria-hidden="true"
               />
-              
+
               {children}
             </div>
           </main>
         </div>
       </body>
     </html>
-  )
+  );
 }
--- dp-game-webapp/app/nav.tsx
+++ dp-game-webapp/app/nav.tsx
@@ -1,27 +1,33 @@
-"use client"
-import React from 'react' 
-import { useState } from 'react'
-import { Dialog } from '@headlessui/react'
-import { Bars3Icon, XMarkIcon, PuzzlePieceIcon } from '@heroicons/react/24/outline'
+"use client";
+import React from "react";
+import Link from "next/link";
+import { useState } from "react";
+import { Dialog } from "@headlessui/react";
+import {
+  Bars3Icon,
+  XMarkIcon,
+  PuzzlePieceIcon,
+} from "@heroicons/react/24/outline";
 
 export default function Header() {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
-    const navigation = [
-        { name: 'About', href: '#' },
-        { name: 'Play', href: '#' },
-    ]
+  const navigation = [
+    { name: "About", href: "/about" },
+    { name: "Play", href: "/game/configure" },
+  ];
 
   return (
     <header className="absolute inset-x-0 top-0 z-50">
-      <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+      <nav
+        className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
+        aria-label="Global"
+      >
         <div className="flex lg:flex-1">
-          <a href="#" className="-m-1.5 p-1.5">
+          <Link href="/" className="-m-1.5 p-1.5">
             <span className="sr-only">Differential Privacy Game</span>
-            <PuzzlePieceIcon
-              className="h-8 w-auto"
-            />
-          </a>
+            <PuzzlePieceIcon className="h-8 w-auto" />
+          </Link>
         </div>
         <div className="flex lg:hidden">
           <button
@@ -35,29 +41,35 @@ export default function Header() {
         </div>
         <div className="hidden lg:flex lg:gap-x-12">
           {navigation.map((item) => (
-            <a key={item.name} href={item.href} className="text-sm font-semibold leading-6 text-gray-900">
+            <a
+              key={item.name}
+              href={item.href}
+              className="text-sm font-semibold leading-6 text-gray-900"
+            >
               {item.name}
             </a>
           ))}
         </div>
         <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-          <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-            Log in <span aria-hidden="true">&rarr;</span>
-          </a>
+          {/* Commenting out until we add login functionality. */}
+          {/*   <a href="#" className="text-sm font-semibold leading-6 text-gray-900"> */}
+          {/*     Log in <span aria-hidden="true">&rarr;</span> */}
+          {/*   </a> */}
         </div>
       </nav>
-      <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
+      <Dialog
+        as="div"
+        className="lg:hidden"
+        open={mobileMenuOpen}
+        onClose={setMobileMenuOpen}
+      >
         <div className="fixed inset-0 z-50" />
         <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
-            <a href="#" className="-m-1.5 p-1.5">
-              <span className="sr-only">Your Company</span>
-              <img
-                className="h-8 w-auto"
-                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                alt=""
-              />
-            </a>
+            <Link href="/" className="-m-1.5 p-1.5">
+              <span className="sr-only">Differential Privacy Game</span>
+              <PuzzlePieceIcon className="h-8 w-auto" />
+            </Link>
             <button
               type="button"
               className="-m-2.5 rounded-md p-2.5 text-gray-700"
@@ -81,17 +93,18 @@ export default function Header() {
                 ))}
               </div>
               <div className="py-6">
-                <a
-                  href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                >
-                  Log in
-                </a>
+                {/* Commenting out until we add login functionality. */}
+                {/* <a */}
+                {/*   href="#" */}
+                {/*   className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50" */}
+                {/* > */}
+                {/*   Log in */}
+                {/* </a> */}
               </div>
             </div>
           </div>
         </Dialog.Panel>
       </Dialog>
     </header>
-  )
+  );
 }
