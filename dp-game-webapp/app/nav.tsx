d7d0eb7d735cdbf9f6d477a083e1e889498fb87e
Erik Taubeneck <erik.taubeneck@gmail.com>
2023-10-17 22:55:16 -0700

    bundler bug with ECMAStript vs CommonJS

--- dp-game-webapp/app/nav.tsx
+++ dp-game-webapp/app/nav.tsx
@@ -1,7 +1,6 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
-import { useState } from "react";
 import { Dialog } from "@headlessui/react";
 import {
   Bars3Icon,
