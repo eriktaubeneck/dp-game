151d9aaa0fe79acc00d0b1b8f3bb877e9d401cdc
Erik Taubeneck <erik.taubeneck@gmail.com>
2023-11-20 09:28:24 -0800

    remove dead code

--- dp-game-webapp/app/about/page.tsx
+++ dp-game-webapp/app/about/page.tsx
@@ -438,20 +438,4 @@ function Toggle({
       />
     </Switch>
   );
-
-  // const handleToggleChange = (_: React.ChangeEvent<HTMLInputElement>) => {
-  //   onChange(!bool);
-  // };
-
-  // return (
-  //   <label className="relative inline-flex items-center cursor-pointer">
-  //     <input
-  //       type="checkbox"
-  //       value="{bool}"
-  //       onChange={handleToggleChange}
-  //       className="sr-only peer"
-  //     />
-  //     <div className="w-[26px] h-3 bg-gray-200 peer-focus:outline-none peer-focus:ring-1 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-3 after:w-3 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-  //   </label>
-  // );
 }
